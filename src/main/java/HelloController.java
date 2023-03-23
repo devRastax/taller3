e4e4dcd62d6a862680030bbe696d829a1000df48
devRastax <andresguevara1601@gmail.com>
2023-03-23 00:06:21 -0500

    segundo_punto

---
+++ src/main/java/HelloController.java
@@ -0,0 +1,12 @@
+package co.edu.uan.sofeng.ejemplo;
+
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.RestController; 
+
+@RestController 
+public class HelloController {
+   @GetMapping("/")
+   public String index() {
+      return "Welcome to Spring Boot!";
+   }
+} 
